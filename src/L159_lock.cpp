993c72f43b49c372bd78813640e5a2840af18096
Naukyz <zhao_yukuan@163.com>
2018-01-09 01:03:18 +0800

    180109

--- src/L159_lock.cpp
+++ src/L159_lock.cpp
@@ -5,3 +5,39 @@ For example,
 Given S = “eceba”,
 T is “ece” which its length is 3.
  */
+#include <string>
+#include <tr1/unordered_map>
+
+using namespace std;
+using namespace tr1;
+
+class Solution {
+public:
+    int lengthOfLongestSubstringTwoDistinct(string s) {
+        int left = 0;
+        int right = 0;
+        int re = 0;
+        unordered_map<char, int> map;
+        while (right < s.length()) {
+            if (map.count(s[right])) {
+                right++;
+                re = max(re, right - left);
+            } else {
+                if (map.size() < 2) {
+                    map[s[right]] = 1;
+                    right++;
+                    re = max(re, right - left);
+                } else {
+                    int a = map[s[left]];
+                    if (a == 1) {
+                        map.erase(s[left]);
+                    } else {
+                        map[s[left]] = a - 1;
+                    }
+                    left++;
+                }
+            }
+        }
+        return re;
+    }
+};
--- src/L3.cpp
+++ src/L3.cpp
@@ -6,3 +6,28 @@ Given "abcabcbb", the answer is "abc", which the length is 3.
 Given "bbbbb", the answer is "b", with the length of 1.
 Given "pwwkew", the answer is "wke", with the length of 3. Note that the answer must be a substring, "pwke" is a subsequence and not a substring.
 */
+#include <string>
+#include <set>
+
+using namespace std;
+
+class Solution {
+public:
+    int lengthOfLongestSubstring(string s) {
+        set<char> set;
+        int left = 0;
+        int right = 0;
+        int re = 0;
+        while (right < s.length()) {
+            if (set.find(s[right]) == set.end()) {
+                set.insert(s[right]);
+                right++;
+                re = max(re, right - left);
+            } else {
+                set.erase(s[left]);
+                left++;
+            }
+        }
+        return re;
+    }
+};
