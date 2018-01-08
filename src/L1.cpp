67c5accc88df49349489f198892828b40c89ef21
Naukyz <zhao_yukuan@163.com>
2018-01-09 00:16:30 +0800

    180108

--- src/L1.cpp
+++ src/L1.cpp
@@ -7,3 +7,31 @@ Given nums = [2, 7, 11, 15], target = 9,
 Because nums[0] + nums[1] = 2 + 7 = 9,
 return [0, 1].
 */
+#include<tr1/unordered_map>
+#include <vector>
+
+using namespace std;
+using namespace tr1;
+
+class IllegalArgumentException {
+};
+
+class Solution {
+public:
+    vector<int> twoSum(vector<int> &nums, int target) {
+        unordered_map<int, int> map;
+        vector<int> re;
+        for (int i = 0; i < nums.size(); ++i) {
+            if (!map.count(nums[i])) {
+                map[nums[i]] = i;
+            }
+            int a = target - nums[i];
+            if (map.count(a) && map[a] != i) {
+                re.push_back(i);
+                re.push_back(map[a]);
+                return re;
+            }
+        }
+        throw IllegalArgumentException();
+    }
+};
