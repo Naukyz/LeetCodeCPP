2e13e44956adf9956708d17334745a9314f6e6e4
Naukyz <zhao_yukuan@163.com>
2018-01-09 00:19:44 +0800

    180109

--- src/L2.cpp
+++ src/L2.cpp
@@ -7,15 +7,13 @@ Input: (2 -> 4 -> 3) + (5 -> 6 -> 4)
 Output: 7 -> 0 -> 8
 Explanation: 342 + 465 = 807.
 */
-#include <cstdio>
-
 const int null = 0;
 
 struct ListNode {
     int val;
     ListNode *next;
 
-    ListNode(int x) : val(x), next(NULL) {}
+    ListNode(int x) : val(x), next(null) {}
 };
 
 class Solution {
